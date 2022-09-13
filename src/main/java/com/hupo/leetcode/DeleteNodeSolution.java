859396a99701cf5c580981b443c91c1b55455414
jwang38 <jwang38@gopay.com.cn>
2022-09-13 17:11:13 +0800

    Revert "LeetCode"
    This reverts commit e3f70c93d445004d957be88408af306e3d04829f.

--- src/main/java/com/hupo/leetcode/DeleteNodeSolution.java
+++ src/main/java/com/hupo/leetcode/DeleteNodeSolution.java
@@ -2,10 +2,11 @@ package com.hupo.leetcode;
 
 public class DeleteNodeSolution {
 
+    private ListNode head;
+
     public void deleteNode(ListNode node) {
         ListNode next = node.next;
         node.val = next.val;
         node.next = next.next;
     }
-
 }
